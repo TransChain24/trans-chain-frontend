0b6f5f49c384c94f86ee24c5e8ba66d7ed091d5f
Aryan49SM <aryansm832@gmail.com>
2024-04-17 19:10:01 +0530

    Order products done with few errors

--- src/pages/auth/sign-up.jsx
+++ src/pages/auth/sign-up.jsx
@@ -41,7 +41,9 @@ export function SignUp() {
       });
 
       if (data.data.status == true) {
-        navigate("/dashborad/home");
+        navigate("/dashboard/home");
+        localStorage.setItem("role",data.data.role);
+        localStorage.setItem("id",data.data.id);
         console.log(data);
       } else {
         console.log("error");
